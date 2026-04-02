d09132d567eec67f36d02ac525a1db886cb09774
Mikhail <playermi888@gmail.com>
2026-04-02 21:27:53 +0300

    1st_try_pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { SocNetRouter } from "./3_sprint/index.js";
-import "./3_sprint/styles/index.css";
+import { SocNetRouter } from "./SocialNetwork_react/index.js";
+import "./SocialNetwork_react/styles/index.css";
 
 function App() {
   return <SocNetRouter />;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import { socNetStore } from "./3_sprint/SocNet";
+import { socNetStore } from "./SocialNetwork_react/SocNet";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={socNetStore}>
-      <BrowserRouter>
+      <BrowserRouter basename="/SocialNetwork_react">
         <App />
       </BrowserRouter>
     </Provider>
