70d62268408cda653627cdac495d8f485ac749bf
Mikhail <playermi888@gmail.com>
2026-04-02 21:39:28 +0300

    Update build

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import { socNetStore } from "./SocialNetwork_react/SocNet";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={socNetStore}>
-      <BrowserRouter basename="/SocialNetwork_react">
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>,
 );
